4dae39df0b2d256393a67cd1bcf0131400861091
kawakami-o3 <mikamihayato383@gmail.com>
2011-12-06 23:24:05 +0900

    Templates and Clean HTML
    Template codes are separated from AirSRM.rb.
    Template format is same as FileEdit.jar.
    IMG tags are removed from HTML files.

--- AirSRM.rb
+++ AirSRM.rb
@@ -7,154 +7,24 @@ require 'mechanize'
 require 'hpricot'
 require 'open-uri'
 require 'rexml/document'
+require 'pp'
 require 'optparse'
 
 XMLFILE  = './tc.xml'
 
-MAINCODE=<<EOS
-eq(NUMBER, (new CLASSNAME()).METHODNAME(PARAMETER), ANSWER);
+MAINBODY=<<'EOS'
+eq($NUMBER$, (new $CLASSNAME$()).$METHODNAME$($PARAMETER$), $ANSWER$);
 EOS
 
-WHOLECODE=<<'EOS'
-import java.util.*;
-public class TestCLASSNAME {
-  public static void main(String[] args) {
-    try {
-MAINCODE
-    } catch( Exception exx) {
-      System.err.println(exx);
-      exx.printStackTrace(System.err);
-    }
-  }
-  private static void eq( int n, int a, int b ) {
-    if ( a==b )
-      System.err.println("Case "+n+" passed.");
-    else {
-      System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
-      throw (new RuntimeException("Wrong answer"));
-    }
-  }
-  private static void eq( int n, char a, char b ) {
-    if ( a==b )
-      System.err.println("Case "+n+" passed.");
-    else {
-      System.err.println("Case "+n+" failed: expected '"+b+"', received '"+a+"'.");
-      throw (new RuntimeException("Wrong answer"));
-    }
-  }
-  private static void eq( int n, long a, long b ) {
-    if ( a==b )
-      System.err.println("Case "+n+" passed.");
-    else {
-      System.err.println("Case "+n+" failed: expected \""+b+"L, received "+a+"L.");
-      throw (new RuntimeException("Wrong answer"));
-    }
-  }
-  private static void eq( int n, boolean a, boolean b ) {
-    if ( a==b )
-      System.err.println("Case "+n+" passed.");
-    else {
-      System.err.println("Case "+n+" failed: expected "+b+", received "+a+".");
-      throw (new RuntimeException("Wrong answer"));
-    }
-  }
-  private static void eq( int n, String a, String b ) {
-    if ( a != null && a.equals(b) )
-      System.err.println("Case "+n+" passed.");
-    else {
-      System.err.println("Case "+n+" failed: expected \""+b+"\", received \""+a+"\".");
-      throw (new RuntimeException("Wrong answer"));
-    }
-  }
-  private static void eq( int n, int[] a, int[] b ) {
-    if ( a.length != b.length ) {
-      System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
-      throw (new RuntimeException("Wrong answer"));
-    }
-    for ( int i= 0; i < a.length; i++)
-      if ( a[i] != b[i] ) {
-        System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
-        print( b );
-        print( a );
-        throw (new RuntimeException("Wrong answer"));
-      }
-    System.err.println("Case "+n+" passed.");
-  }
-  private static void eq( int n, long[] a, long[] b ) {
-    if ( a.length != b.length ) {
-      System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
-      throw (new RuntimeException("Wrong answer"));
-    }
-    for ( int i= 0; i < a.length; i++ )
-      if ( a[i] != b[i] ) {
-        System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
-        print( b );
-        print( a );
-        throw (new RuntimeException("Wrong answer"));
-      }
-    System.err.println("Case "+n+" passed.");
-  }
-  private static void eq( int n, String[] a, String[] b ) {
-    if ( a.length != b.length) {
-      System.err.println("Case "+n+" failed: returned "+a.length+" elements; expected "+b.length+" elements.");
-      throw (new RuntimeException("Wrong answer"));
-    }
-    for ( int i= 0; i < a.length; i++ )
-      if( !a[i].equals( b[i])) {
-        System.err.println("Case "+n+" failed. Expected and returned array differ in position "+i);
-        print( b );
-        print( a );
-        throw (new RuntimeException("Wrong answer"));
-      }
-    System.err.println("Case "+n+" passed.");
-  }
-  private static void print( int a ) {
-    System.err.print(a+" ");
-  }
-  private static void print( long a ) {
-    System.err.print(a+"L ");
-  }
-  private static void print( String s ) {
-    System.err.print("\""+s+"\" ");
-  }
-  private static void print( int[] rs ) {
-    if ( rs == null) return;
-    System.err.print('{');
-    for ( int i= 0; i < rs.length; i++ ) {
-      System.err.print(rs[i]);
-      if ( i != rs.length-1 )
-        System.err.print(", ");
-    }
-    System.err.println('}');
-  }
-  private static void print( long[] rs) {
-    if ( rs == null ) return;
-    System.err.print('{');
-    for ( int i= 0; i < rs.length; i++ ) {
-      System.err.print(rs[i]);
-      if ( i != rs.length-1 )
-        System.err.print(", ");
-    }
-    System.err.println('}');
-  }
-  private static void print( String[] rs ) {
-    if ( rs == null ) return;
-    System.err.print('{');
-    for ( int i= 0; i < rs.length; i++ ) {
-      System.err.print( "\""+rs[i]+"\"" );
-      if( i != rs.length-1)
-        System.err.print(", ");
-    }
-    System.err.println('}');
-  }
-}
-EOS
+TESTCODE=open("./template/TestTemplate.java",'r').read
+YOURCODE=open("./template/CodeTemplate.java",'r').read
 
 
 
 class AirSRM
 
   def initialize options
+    @overwrite = options[:force]
     @srm = options[:srm].to_i
     @div = options[:div].to_i
     @level = options[:level].to_i
@@ -162,16 +32,21 @@ class AirSRM
       a.user_agent_alias = 'Mechanize'
     end
 
+
     @rd = getRoundId()
     @problem = getProblem()
     @pm = @problem[:id]
     @fnStatement = "#{@problem[:name]}.html"
     @fnParameter = "#{@problem[:name]}.systemtest.html"
     @fnTestCode = "Test#{@problem[:name]}.java"
+    @fnYourCode = "#{@problem[:name]}.java"
   end
 
   def login
+    return if @agent.cookie_jar.jar[".topcoder.com"]
+    print "Login to TopCoder.com ... "
     page = @agent.get('https://community.topcoder.com/tc?&module=Login')
+
     sys_form = page.form("frmLogin")
     sys_form.field_with(:name => 'username').value = USERNAME
     sys_form.field_with(:name => 'password').value = PASSWORD
@@ -179,15 +54,19 @@ class AirSRM
     page = @agent.submit(sys_form)
 
     unless page.body.index("Forgot") == nil
-      puts "login error"
+      puts "Login error"
       exit
     end
+    puts "Done."
   end
 
   def getRoundId
-    unless File.exist?(XMLFILE)
-      #puts "saving a round list"
-      open(XMLFILE,'w').write(@agent.get("http://www.topcoder.com/tc?module=BasicData&c=dd_round_list").body)
+    if (not File.exist?(XMLFILE)) or (@overwrite)
+      print "Saving a round list ... "
+      open(XMLFILE,'w') do |file|
+        file.write(@agent.get("http://www.topcoder.com/tc?module=BasicData&c=dd_round_list").body)
+      end
+      puts "Done."
     end
     roundList = REXML::Document.new(File.open(XMLFILE,'r').read)
 
@@ -217,15 +96,18 @@ class AirSRM
 
   def saveStatement
 
-    if File.exist?(@fnStatement)
+    if File.exist?(@fnStatement) and (not @overwrite)
       puts "The problem statement, #{@fnStatement}, exists."
-      exit
+      return
     end
       
     login()
-    page = @agent.get("http://community.topcoder.com/stat?c=problem_statement&pm=#{@pm}&rd=#{@rd}")
+    origin = "http://community.topcoder.com/stat?c=problem_statement&pm=#{@pm}&rd=#{@rd}"
+    page = @agent.get(origin)
     flag = false
-    cnt = ["<html><body>"]
+    cnt = ["<html><body>",
+           "<p><a href=\"#{origin}\">#{page.body.scan(/<a .*?ProblemDetail.*?>(.*?)<\/a>/).flatten.first}</a> on community.topcoder.com</p>"
+    ]
     page.body.split("\n").each do |i|
       if i =~ /BEGIN BODY/
         flag = true
@@ -236,14 +118,28 @@ class AirSRM
     end
     cnt << "</body></html>"
 
-    puts "saving the statement, #{@fnStatement} ..."
-    open(@fnStatement,"w").write(cnt.join("\n"))
+    cnt = cnt.join("\n")
+    cnt.gsub!(/<IMG.*?>/,'')
+    cnt.gsub!(/BACKGROUND=".*?"/,'')
+    cnt.gsub!(/<TD.*?>\s*?<\/TD>/,'')
+    cnt.gsub!(/<TD.*?bodyTextBold.*?>.*?<\/TD>/m,'')
+    cnt.gsub!(/<TR.*?>\s*?<\/TR>/,'')
+    cnt.gsub!(/<TABLE.*?>\s*?<\/TABLE>/,'')
+    cnt.gsub!(/^\s*$/,'')
+    cnt.gsub!(/\n+/,"\n")
+
+
+    print "Saving the statement, #{@fnStatement} ... "
+    open(@fnStatement,"w") do |file|
+      file.write(cnt)
+    end
+    puts "Done."
   end
 
   def saveSystemTestParam
-    if File.exist?(@fnParameter)
+    if File.exist?(@fnParameter) and (not @overwrite)
       puts "The test cases, #{@fnParameter}, exist."
-      exit
+      return
     end
 
     login()
@@ -258,7 +154,9 @@ class AirSRM
     page = @agent.get(solutions.first)
 
     flag = false
-    cnt = ["<html><body>"]
+    cnt = ["<html><body>",
+           "<p><a href=\"http://community.topcoder.com/stat?c=round_stats_sorted&rd=#{@rd}&dn=#{@div}&sq=Round_Statistics_Data&sc=10&sd=desc\">Match Result</a> on community.topcoder.com</p>",
+    "<p>Parameters and expected results</p>"]
     page.body.split("\n").each do |i|
       if i =~ /- System/
         flag = true
@@ -271,18 +169,24 @@ class AirSRM
 
 
     cnt = cnt.join("\n")
-    cnt = cnt.gsub(/BGCOLOR=".*?"/,'').gsub(/BACKGROUND=".*?"/,'').
-      gsub(/<IMG.*?>/,'').
-      gsub(/<TD.*?>\s*?<\/TD>/,'').
-      gsub(/<TD.*?>Passed<\/TD>/,'').
-      gsub(/<TD.*?middle.*?>.*?<\/TD>/m,'').
-      gsub(/<TD.*?statTextBig.*?>.*?<\/TD>/m,'').
-      gsub(/<TR.*?>\s*?<\/TR>/,'').
-      gsub(/^\s*$/,"").gsub(/\n+/,"\n").sub(/BORDER="0"/,'BORDER="1"')
+    cnt.gsub!(/BGCOLOR=".*?"/,'')
+    cnt.gsub!(/BACKGROUND=".*?"/,'')
+    cnt.gsub!(/<IMG.*?>/,'')
+    cnt.gsub!(/<TD.*?>\s*?<\/TD>/,'')
+    cnt.gsub!(/<TD.*?>Passed<\/TD>/,'')
+    cnt.gsub!(/<TD.*?middle.*?>.*?<\/TD>/m,'')
+    cnt.gsub!(/<TD.*?statTextBig.*?>.*?<\/TD>/m,'')
+    cnt.gsub!(/<TR.*?>\s*?<\/TR>/,'')
+    cnt.gsub!(/^\s*$/,'')
+    cnt.gsub!(/\n+/,"\n")
+    cnt.sub!(/BORDER="0"/,'BORDER="1"')
 
 
-    puts "saving test cases, #{@fnParameter} ..."
-    open(@fnParameter,"w").write(cnt)
+    print "Saving test cases, #{@fnParameter} ... "
+    open(@fnParameter,"w") do |file|
+      file.write(cnt)
+    end
+    puts "Done."
   end
 
   def getDefinition fn
@@ -297,58 +201,101 @@ class AirSRM
     ret
   end
 
-  def getParameters fn
-    (Hpricot(open(fn,'r').read)/:tr).map {|i| (i/:td).map {|i| i.inner_html}}
-  end
 
   def genMainCode n,definition, parameter
     parText = definition[:parameters].split(",").zip(parameter[0].split("\n")).map do |i,j|
       i =~ /\[\]/ ? "new #{i}#{j}" : j
     end.join
-    
-    ret = String.new(MAINCODE)
-    ret.sub!(/PARAMETER/,parText)
-    ret.sub!(/NUMBER/,n.to_s)
-    ret.sub!(/CLASSNAME/,definition[:class])
-    ret.sub!(/METHODNAME/,definition[:method])
-    ret.sub!(/ANSWER/,parameter[1])
-    ret.gsub!(/,/,",\n")
+
+    ret = String.new(MAINBODY)
+    ret.sub!(/\$NUMBER\$/,n.to_s)
+    ret.sub!(/\$CLASSNAME\$/,definition[:class])
+    ret.sub!(/\$METHODNAME\$/,definition[:method])
+    ret.sub!(/\$PARAMETER\$/,parText)
+    ret.sub!(/\$ANSWER\$/,parameter[1])
+    ret.gsub!(/",/,"\",\n")
     ret
   end
 
-  def genWholeCode definition,parameters
-    main = ""
+  def genYourCode definition
+    cnt = open(@fnStatement,"r").read.gsub(/<pre>/,'').gsub(/<\/pre>/,'').gsub(/&quot;/,"\"")
+    cnt = Hpricot(cnt)/:table/:tr/:td/:table/:tr/:td/:table/:tr/:td/:table/:tr/:td
+    
+    arr = (cnt/:table/:tr/:td).map {|i| i.inner_html}
+    params = arr.delete_if {|i| i=~/\.$/}
+   
+    arr = cnt.map {|i| i.inner_html}
+    returns = arr.delete_if {|i| not i=~/Returns: /}.map{|i| i.sub(/Returns: /,'')}
+
+    arr = cnt.map {|i| i.inner_html}
+    methodparms = arr[arr.index("Method signature:")+1].scan(/\((.*?)\)/).flatten.first
+
+    np = params.length / returns.length
+    main = ''
+    returns.length.times do |i|
+      main += genMainCode(i,definition,[params[np*i..np*i+np-1].join(",\n"),returns[i]])
+    end
+
+    ret = String.new(YOURCODE)
+    ret.sub!(/\$MAINBODY\$/,main)
+    ret.gsub!(/\$CLASSNAME\$/,definition[:class])
+    ret.gsub!(/\$RC\$/,definition[:returns])
+    ret.gsub!(/\$METHODNAME\$/,definition[:method])
+    ret.gsub!(/\$METHODPARMS\$/,methodparms)
+    ret.gsub!(/\$BEGINCUT\$/,'// BEGIN CUT HERE')
+    ret.gsub!(/\$ENDCUT\$/,'// END CUT HERE')
+    ret.gsub!(/\$PROBLEMDESC\$/,'')
+    ret.gsub!(/\$WRITERCODE\$/,'')
+    ret
+  end
+
+  def saveYourCode
+    if File.exist?(@fnYourCode) # not depend on "force" option.
+      puts "#{@fnYourCode} exists."
+      return
+    end
+
+    df = getDefinition(@fnStatement)
+    print "Saving #{@fnYourCode} ... "
+    open(@fnYourCode,"w") do |file|
+      file.write(genYourCode(df))
+    end
+    puts "Done."
+  end
+
+  def genTestCode definition,parameters
+    main = ''
     parameters.length.times do |i|
       main += genMainCode(i,definition,parameters[i])
     end
    
-    ret = String.new(WHOLECODE)
-    ret.sub!(/CLASSNAME/,@problem[:name])
-    ret.sub!(/MAINCODE/,main)
+    ret = String.new(TESTCODE)
+    ret.sub!(/\$CLASSNAME\$/,@problem[:name])
+    ret.sub!(/\$MAINBODY\$/,main)
     ret
   end
 
 
   def saveSystemTest
-    if File.exist?(@fnTestCode)
+    def getParameters fn
+      (Hpricot(open(fn,'r').read)/:tr).map {|i| (i/:td).map {|i| i.inner_html}}
+    end
+    if File.exist?(@fnTestCode) and (not @overwrite)
       puts "Test code, #{@fnTestCode}, exists."
-      exit
+      return
     end
 
-
-    unless File.exist?(@fnStatement)
-      saveStatement
-    end
+    saveStatement
     df = getDefinition(@fnStatement)
 
-    unless File.exist?(@fnParameter)
-      saveSystemTestParam
-    end
+    saveSystemTestParam
     pa = getParameters(@fnParameter)
 
-    print "Generating #{@fnTestCode} ..."
-    open(@fnTestCode,"w").write(genWholeCode(df,pa))
-    puts " Done"
+    print "Saving #{@fnTestCode} ... "
+    open(@fnTestCode,"w") do |file|
+      file.write(genTestCode(df,pa))
+    end
+    puts "Done."
   end
 
 
@@ -356,13 +303,15 @@ end
 
 
 options = {}
+options[:systemtest] = false
+options[:force] = false
 optparse = OptionParser.new do |opts|
   opts.on('--systemtest') do
     options[:systemtest] = true
   end
-# opts.on('-f','--force') do
-#   options[:xml] = true
-# end
+  opts.on('-f','--force') do
+    options[:force] = true
+  end
   opts.on('--srm=VAL') do |v|
     options[:srm] = v
   end
@@ -390,6 +339,8 @@ optparse.parse!
 if (options[:systemtest])
   AirSRM.new(options).saveSystemTest
 else
-  AirSRM.new(options).saveStatement
+  tmp = AirSRM.new(options)
+  tmp.saveStatement
+  tmp.saveYourCode
 end
 
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # これは何?
-Topcoder の過去問およびシステムテストの内容をダウンロードするスクリプトです。また、システムテストを行えるように、テストコードを生成します。
+Topcoder SRM 練習支援スクリプトです。主な機能は
+
+* 問題文のダウンロード
+* 雛形コードの生成（[FileEdit.jar](http://community.topcoder.com/contest/classes/FileEdit/FileEdit.htm)プラグイン形式に準拠）
+* システムテストで使用されるパラメータのダウンロード
+* システムテスト実行コードの生成
+
 
 # 使い方は?
 まず、AirSRM.rb をエディタで開いて、ユーザ名とパスワードを編集してください。
@@ -7,11 +13,17 @@ Topcoder の過去問およびシステムテストの内容をダウンロー�
 	USERNAME = 'hogehoge'
 	PASSWORD = 'foobar'
 
-問題文をダウンロードするには3つのオプションが必要です。例えば、SRM 525 の Div 2 Easy をダウンロードしたい場合、ラウンド回の 525、Division の 2、難易度の 1 (Easy) となります。実行例は、
+問題文をダウンロードするには3つのオプションが必要です。例えば、SRM 525 の Div 2 Easy をダウンロードしたい場合、
+
+* ラウンド回の「525」
+* Division の「2」
+* 難易度の「1」(Easy)
+
+ となります。実行例は、
 
 	% ruby AirSRM.rb --srm=525 --division=2 --level=1
 
-SRM 525 Div2 Easy の問題名は RainyRoad なので、問題文は RainyRoad.html として保存されます。難易度のオプションについては、Medium なら 2、Hard なら 3 を与えてください。また、divやlvのように省略することもでき、以下のように実行することも出来ます。
+SRM 525 Div2 Easy の問題名は RainyRoad なので、問題文は RainyRoad.html として保存され、雛形コードは RainyRoad.java として生成されます。難易度のオプションについては、Medium なら 2、Hard なら 3 を与えてください。また、divやlvのように省略することもでき、以下のように実行することも出来ます。
 
 	% ruby AirSRM.rb --srm=525 --div=2 --lv=1
 	
@@ -25,3 +37,14 @@ SRM 525 Div2 Easy の問題名は RainyRoad なので、問題文は RainyRoad.h
 
 一つでも返り値が間違っていた場合は、"Wrong Answer"のランタイムエラーを吐いて終了します。
 なお、TLEやMLEは判定していませんし、返り値がdoubleの場合はエラーとなるため、注意してください。
+
+# 他には?
+
+ウェブアクセス軽減のために、ラウンドリストを tc.xml として保存しています。
+これは [Algoritm Data Feeds](http://apps.topcoder.com/wiki/display/tc/Algorithm+Data+Feeds) で
+公開されている Argorithm Round List と同じものです。最新のラウンドリストを取得したい場合は、
+tc.xmlを削除するか、-fオプションを指定して実行してください。
+
+	% ruby AirSRM.rb --srm=525 --division=2 --level=1 -f
+
+その他の情報については、[github wiki](https://github.com/kawakami-o3/AirSRM/wiki) を参照してください。
